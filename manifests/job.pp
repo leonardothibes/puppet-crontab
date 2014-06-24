f94306f1b03950f36b0436a77d3b7d3046e60a9e
Leonardo Thibes <eu@leonardothibes.com>
2014-06-24 14:21:29 -0300

    adicionando validacoes

--- manifests/job.pp
+++ manifests/job.pp
@@ -14,10 +14,23 @@ define crontab::job(
 ) {
 	include crontab::params
 	include crontab
-	case $ensure {
-		'present','absent': { $real_ensure = $ensure }
-		default           : { fail("Invalid value \"${ensure}\" used for ensure") }
+	
+	if !(($ensure == 'present') or ($ensure == 'absent')) {
+		fail("Invalid value \"${ensure}\" used for ensure param")
 	}
+	if !($minute == '*' or ($minute >= 0 and $minute <= 59)) {
+		fail('Specify a minute in the range 0-59')
+	}
+	if !($hour == '*' or ($hour >= 0 and $hour <= 23)) {
+		fail('Specify an hour in the range 0-23')
+	}
+	if !($monthday == '*' or ($monthday >= 1 and $monthday <= 31)) {
+		fail('Specify a day of month in the range 1-31')
+	}
+	if !($month == '*' or ($month >= 1 and $month <= 12)) {
+		fail('Specify a month in the range 1-12')
+	}
+
 	file {"cronjob::${title}":
 		ensure  => $real_ensure,
 		owner   => $user,
